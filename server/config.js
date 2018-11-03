6170a60a5db3e14b995a3026b9ec56546dae1389
makozi <marizumakozi97@gmail.com>
2018-11-03 13:24:01 +0100

    update

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    api_key: 'API_KEY', //put in your api key
-    api_secret: 'API_SECRET',  //  Put in your Api Secret
-    number: 'NUMBER' //put in your number
-  };
+  api_key: '64f4eb26', //put in your api key
+  api_secret: 'xhTM6Td4Ed8L6meL',  //  Put in your Api Secret
+  number: '2348173827638' //put in your number
+
+};
+
